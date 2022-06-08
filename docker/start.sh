4b26a3ac28fb7c927a3f621a7752975a2fcfaefd
nabr <100725601+nabr2730@users.noreply.github.com>
2022-06-08 13:09:49 +0900

    update: 関連パッケージのインストールをDockerfileで完結させるように

--- docker/start.sh
+++ docker/start.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 set -e
 
-# むりやり日本サーバーに切り替える
-echo "160.26.2.187 archive.ubuntu.com" | tee -a /etc/hosts
-echo "160.26.2.187 security.ubuntu.com" | tee -a /etc/hosts
-# 各種インストール
-TZ=Asia/Tokyo
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt update && apt upgrade -y
-apt install -y python3-pip
-apt install ffmpeg -y
-apt-get install -y software-properties-common \
-    && add-apt-repository ppa:git-core/ppa \
-    && apt-get update -y
-apt-get install -y git 
-
-
-
-
 # configがなかったら起動を停止
-git --version
-git clone https://github.com/NamagomiNetwork/ace-attorney-discord-bot.git
-cd ace-attorney-discord-bot
-cp /config.yaml /ace-attorney-discord-bot/config.yaml
-if [ ! -e config.yaml ];then
+if [ ! -e /config.yaml ];then
     echo "config.yaml が見つかりません。"
     echo "configFileをdocker-composeなどで /config.yaml にマウントしてください"
     echo "configファイルのサンプルは https://github.com/NamagomiNetwork/ace-attorney-discord-bot/blob/main/config.yaml.example を参照してください"
     exit 1
 fi
 
-python3 -m pip install -r requirements.txt
+cp /config.yaml /ace-attorney-discord-bot/config.yaml
+cd /ace-attorney-discord-bot || exit
+
 python3 main.py
